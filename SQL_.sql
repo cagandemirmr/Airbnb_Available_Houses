dd640b981abf8b010ed3d51ce86d8bd983c808fb
Cagan Demir <145290776+cagandemirmr@users.noreply.github.com>
2024-12-20 12:37:22 +0300

    Add files via upload

---
+++ SQL_.sql
@@ -0,0 +1,18 @@
+select COUNT(*) from listings
+
+
+select  * from listings a
+join reviews b on a.id = b.listing_id where a.host_is_superhost is not null and
+a.description is not null and a.bathrooms is not null and a.beds is not null 
+and a.price is not null and price is not null and first_review is not null
+
+
+
+select top 1000 * from listings 
+
+
+drop table calender
+
+
+
+
